95c4564de288a780817d545cc090b1e680885985
Brandon Blanker Lim-it <flamendless8@gmail.com>
2022-07-25 23:20:04 +0800

    Add debug volume display

--- menu.lua
+++ menu.lua
@@ -771,6 +771,10 @@ function Menu:draw()
             btn:draw()
         end
     end
+
+    love.graphics.setColor(1, 0, 0, 1)
+    love.graphics.print(tostring(UserData.data.main_volume), 32, 32)
+    love.graphics.setColor(1, 1, 1, 1)
 end
 
 function Menu:mousepressed(mx, my, mb)
